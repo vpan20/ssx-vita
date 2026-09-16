ee86f22d80a467de65ff31c4aa5d74688f525e3e
vpan20 <vivekmpandya@gmail.com>
2026-09-16 16:48:10 -0700

    Update so_util.c

--- loader/so_util.c
+++ loader/so_util.c
@@ -350,8 +350,10 @@ int so_resolve(so_module *mod, so_default_dynlib *default_dynlib, int size_defau
 
 void so_initialize(so_module *mod) {
   for (int i = 0; i < mod->num_init_array; i++) {
-    if (mod->init_array[i])
+    if (mod->init_array[i]) {
+      debugPrintf("init[%d/%d] off=0x%X\n", i, mod->num_init_array, (unsigned)((uintptr_t)mod->init_array[i] - mod->text_base));
       mod->init_array[i]();
+    }
   }
 }
 
